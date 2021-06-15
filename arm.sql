beab1eb498a7d897f3368c4fb0d81efbdf766d36
RashaAlharthi <russa.m99@gmail.com>
2021-06-16 01:35:18 +0300

    Create arm.sql

---
+++ arm.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 16, 2021 at 12:34 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `robot_arm`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `arm`
+--
+
+CREATE TABLE `arm` (
+  `motor1` int(11) NOT NULL,
+  `motor2` int(11) NOT NULL,
+  `motor3` int(11) NOT NULL,
+  `motor4` int(11) NOT NULL,
+  `motor5` int(11) NOT NULL,
+  `motor6` int(11) NOT NULL,
+  `power` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
