067aba68ec39e59318c3546cfbd9e19aa5846c82
RashaAlharthi <russa.m99@gmail.com>
2021-06-16 13:24:16 +0300

    update the function to display value of each slider

--- Arm1/sc.js
+++ Arm1/sc.js
@@ -1,7 +1,12 @@
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value;
+function updateLabel() {
+  var limit = this.parentElement.getElementsByClassName("limit")[0];
+  limit.innerHTML = this.value;
+}
 
-slider.oninput = function() {
-  output.innerHTML = this.value;
+var slideContainers = document.getElementsByClassName("slidecontainer");
+
+for (var i = 0; i < slideContainers.length; i++) {
+  var slider = slideContainers[i].getElementsByClassName("slider")[0];
+  updateLabel.call(slider);
+  slider.oninput = updateLabel;
 }
