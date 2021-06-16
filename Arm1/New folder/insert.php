a08a49686b09857895fb0f5cb85ea2fac4e64653
salha77 <60719077+falk20-00@users.noreply.github.com>
2021-06-17 02:57:47 +0300

    Update insert.php

--- Arm1/New folder/insert.php
+++ Arm1/New folder/insert.php
@@ -5,19 +5,17 @@
 </head>
 <body>
       <?php
-	  $value1 = $_POST['value1'];
-	  $value2  = $_POST['value2'];
-	  $value3  = $_POST['value3'];
-	  $value4  = $_POST['value4'];
-	  $value5 = $_POST['value5'];
-	  $value6  = $_POST['value6'];
-	  $value7 = $_POST['value7'];
+	  $Base = $_POST['Base'];
+	  $Shoulder = $_POST['Shoulder'];
+	  $Elbow = $_POST['Elbow'];
+	  $Wrist  = $_POST['Wrist:'];
+	  $Gripper = $_POST['Gripper'];
+	  $On = $_POST['On'];
        $conn= new mysqli("localhost","root","","robot_arm");
-	   $stmt=$conn->prepare("insert into arm (motor1,motor2,motor3,motor4,motor5,motor6,power)values(?,?,?,?,?,?,?);");
-      $stmt=->bind_param("iiiiiii", $value1 ,$value2,$value3,$value4,$value5,$value6,$value7);
+	   $stmt=$conn->prepare("insert into arm (motor1,motor2,motor3,motor4,motor5,power)values(?,?,?,?,?,?);");
+      $stmt=->bind_param("iiiiii",$Base,$Shoulder,$Elbow,$Wrist,$Gripper,$On );
 	  $stst->execute();
 	  echo "Done" ;
 	  ?>
 </body>
 </html>
-
