6e0f80ac28c44507bc6c6ba483276949bb32b9fc
shahadNa1 <50388183+shahadNa1@users.noreply.github.com>
2021-06-19 01:45:07 +0300

    Add files via upload

--- arm.sql
+++ arm.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 16, 2021 at 12:34 AM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 8.0.1
+-- Generation Time: 19 يونيو 2021 الساعة 00:41
+-- إصدار الخادم: 10.4.19-MariaDB
+-- PHP Version: 7.3.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,18 +24,38 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `arm`
+-- بنية الجدول `arm`
 --
 
 CREATE TABLE `arm` (
-  `motor1` int(11) NOT NULL,
-  `motor2` int(11) NOT NULL,
-  `motor3` int(11) NOT NULL,
-  `motor4` int(11) NOT NULL,
-  `motor5` int(11) NOT NULL,
-  `motor6` int(11) NOT NULL,
-  `power` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+  `Id` int(11) NOT NULL,
+  `Power` int(11) NOT NULL,
+  `Motor1` int(11) NOT NULL,
+  `Motor2` int(11) NOT NULL,
+  `Motor3` int(11) NOT NULL,
+  `Motor4` int(11) NOT NULL,
+  `Motor5` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `arm`
+--
+ALTER TABLE `arm`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `arm`
+--
+ALTER TABLE `arm`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
